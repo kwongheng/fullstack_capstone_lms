4cf7c024dfec14bc66d2e072a712f18573adab2d
Kelvin Wong <kwongheng@gmail.com>
2025-12-03 12:05:20 +0800

    Update security to allow everyone in DEV

--- springboot/lms/src/main/java/com/ntuc/lms/config/SecurityConfig.java
+++ springboot/lms/src/main/java/com/ntuc/lms/config/SecurityConfig.java
@@ -2,15 +2,26 @@ package com.ntuc.lms.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 //config/SecurityConfig.java
 @Configuration
 public class SecurityConfig {
 
- @Bean
- public PasswordEncoder passwordEncoder() {
-     return new BCryptPasswordEncoder();
- }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll() // ← allow everything
+		).csrf(csrf -> csrf.disable()) // needed for Postman PUT/POST/DELETE
+				.httpBasic(Customizer.withDefaults()).formLogin(Customizer.withDefaults());
+		return http.build();
+	}
 }
