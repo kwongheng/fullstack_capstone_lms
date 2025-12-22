d9acc36adb3c099bd7d87115ae92f1e1bc0a264f
Kelvin Wong <kwongheng@gmail.com>
2025-12-22 10:27:47 +0800

    FIX -> Update CORS sequence

--- springboot/lms/src/main/java/com/ntuc/lms/config/SecurityConfig.java
+++ springboot/lms/src/main/java/com/ntuc/lms/config/SecurityConfig.java
@@ -14,9 +14,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ntuc.lms.security.JwtAuthenticationFilter;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 @Configuration
@@ -43,6 +47,19 @@ public class SecurityConfig {
         };
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
+    }    
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http,
                                                     JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
